08c5b7d1c89064c767aa3e51718073611f720d02
Jean-Jacques ETUNE NGI <jeanjacques.etunengi@adservio.fr>
2019-08-20 20:56:00 +0200

    Implementation des tests

--- molecule/certbot_auto_install/tests/test_default.py
+++ molecule/certbot_auto_install/tests/test_default.py
@@ -9,24 +9,28 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_certbot_installed(host):
 
     # Certbot command path
-    command_path = '/usr/bin/certbot'
+    command_path = '/usr/local/bin/certbot-auto'
 
+    # Certbot command link path
+    command_link_path = '/usr/local/bin/certbot-auto'
+    
     # Check that command exists
     assert host.file(command_path).exists
 
     # Check that command is file
     assert host.file(command_path).is_file
 
+    # Check that command link exists
+    assert host.file(command_link_path).exists
+
+    # Check that command link is symlink
+    assert host.file(command_link_path).is_symlink
+
+    # Check symlink target
+    assert host.file(command_link_path).linked_to == command_path
+
     # Execute command and get result
-    command_run = host.run(command_path + ' --version')
+    command_run = host.run(command_link_path)
 
     # Assert that run is OK
     assert command_run.rc == 0
-
-    # Get the running version
-    version = command_run.stderr\
-                         .split('\n')[0]\
-                         .split(' ')[1]
-
-    # Assert version is not empty
-    assert len(version) > 0
